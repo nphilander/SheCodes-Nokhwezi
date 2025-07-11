491788be8f9be1af02525d671b5044028d2d3d81
nokhwezi <nokhwezi13@gmail.com>
2025-07-11 04:29:31 +0200

    added fucntionality with ajax axios for weather api call for the city and the city's temperature

--- Coding editor/SheCodes Plus/Homework 1/script.js
+++ Coding editor/SheCodes Plus/Homework 1/script.js
@@ -16,22 +16,12 @@ function updateCurrentDate() {
   return `${dayName} ${hours}:${minutes}`;
 }
 
-let currentWeatherInfo = document.querySelector(".current-weather p");
-let cityDisplay = document.querySelector(".current-weather h1");
-let searchInput = document.querySelector(".search-input");
-let searchButton = document.querySelector(".search-button");
-
-currentWeatherInfo.innerHTML = `
-  ${updateCurrentDate()}, slightly cloudy <br />
-  Humidity: <strong>67%</strong> Wind: <strong>6.1km/h</strong>
-`;
-
 function handleSearch(event) {
   event.preventDefault();
   let cityName = searchInput.value.trim();
 
   if (cityName) {
-    cityDisplay.textContent = cityName;
+    findCity(cityName);
     searchInput.value = "";
   }
 }
@@ -41,6 +31,34 @@ function handleKeyPress(event) {
     handleSearch(event);
   }
 }
+function displayTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let city = response.data.city;
+
+  let tempElement = document.querySelector(".temp");
+  tempElement.innerHTML = `☀️ ${temperature}&deg C`;
+
+  cityDisplay.textContent = city;
+}
+
+function findCity(city) {
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+let currentWeatherInfo = document.querySelector(".current-weather p");
+let cityDisplay = document.querySelector(".current-weather h1");
+let searchInput = document.querySelector(".search-input");
+let searchButton = document.querySelector(".search-button");
+let apiKey = "ob2cf76b441t303bab3624054158f86a";
+
+currentWeatherInfo.innerHTML = `
+    ${updateCurrentDate()}, slightly cloudy <br />
+    Humidity: <strong>67%</strong> Wind: <strong>6.1km/h</strong>
+  `;
 
 searchButton.addEventListener("click", handleSearch);
 searchInput.addEventListener("keypress", handleKeyPress);
+
+//default city
+findCity("Pretoria");
